3fd7beab994febc46fe78b05b8d0c22c89ef9a21
Mauro <mauro.segovia.7@gmail.com>
2017-05-21 23:11:44 -0300

    Definición de una clase y prototipos

--- PlatziJS/Objetos/distancia_puntos.js
+++ PlatziJS/Objetos/distancia_puntos.js
@@ -1,22 +1,34 @@
-const p1 = {
-  x: 0,
-  y: 4,
-  moverEnX: function (x){ this.x += x },
-  moverEnY: function (y){ this.y += y }
+//Esta funcion la utilizo para crear puntos en forma de objeto
+//este bloque se llama constructor
+function Punto(x, y){
+  this.x = x
+  this.y = y
+  //No hace falta poner return porque js ya lo hace
 }
 
-const p2 = {
-  x: 3,
-  y: 0,
-  moverEnX: function (x){ this.x += x },
-  moverEnY: function (y){ this.y += y }
+//Los metodos para la clase se definen de esta forma.
+//Afectara a todos los objetos
+Punto.prototype.moverEnX = function moverEnX(x) { this.x += x }
+Punto.prototype.moverEnY = function moverEnY(y) { this.y += y }
+Punto.prototype.distancia = function distancia(p) {
+  const x = this.x - p.x
+  const y = this.y - p.y
+
+  return Math.sqrt(x * x + y * y)
 }
 
-function distancia(p1, p2){
-  const x = p1.x - p2.x
-  const y = p1.y - p2.y
 
-  return Math.sqrt(x * x + y * y);
-}
+//Así se crean los objetos con el prototipo de la función
+const p2 = new Punto(3, 0)
+const p1 = new Punto(0, 4)
 
-console.log(distancia(p1, p2));
+
+
+console.log(p1.distancia(p2));
+console.log(p2.distancia(p1))
+p1.moverEnX(10);
+console.log(p1.distancia(p2));
+p2.moverEnY(-4);
+console.log(p1.distancia(p2));
+
+/* Los objetos están basados en prototipos, cada cambio en el prototipo creará cambios en el resto de los objetos
